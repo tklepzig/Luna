e93e40261fb9335ba3e94b0583d54a9a3bc8507c
Thomas <Thomas@DevBook>
2017-02-26 17:46:35 +0100

    es6 for server

--- hub/server/index.js
+++ hub/server/index.js
@@ -1,70 +1,69 @@
-'use strict';
+import express from "express";
+import http from "http";
+import socketIo from "socket.io";
+import path from "path";
 
-var port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
+const app = express();
+const httpServer = http.createServer(app);
+const socketIoServer = socketIo.listen(httpServer);
 
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var socketIo = require('socket.io')(http);
-var path = require('path');
+app.use("/", express.static(path.resolve(__dirname + "/../public")));
 
-
-app.use('/', express.static(path.resolve(__dirname + "/../public")));
-
-socketIo.on('connection', function (socket) {
-    var clientIp = socket.request.connection.remoteAddress;
-    console.log('Client connected:\t' + clientIp);
-    socket.on('disconnect', function () {
-        console.log('Client disconnected:\t' + clientIp);
+socketIoServer.on("connection", socket => {
+    const clientIp = socket.request.connection.remoteAddress;
+    console.log("Client connected:\t" + clientIp);
+    socket.on("disconnect", () => {
+        console.log("Client disconnected:\t" + clientIp);
     });
 
     //client functions
-    socket.on("ping", function (pong) {
+    socket.on("ping", pong => {
         pong();
     });
 
-    socket.on('media-playPause', function (id) {
+    socket.on("media-playPause", id => {
         socket.broadcast.emit("media-playPause", id);
     });
 
-    socket.on('media-volumeUp', function (id) {
+    socket.on("media-volumeUp", id => {
         socket.broadcast.emit("media-volumeUp", id);
     });
 
-    socket.on('media-volumeDown', function (id) {
+    socket.on("media-volumeDown", id => {
         socket.broadcast.emit("media-volumeDown", id);
     });
 
-    socket.on('keyboard-pressKey', function (id, key, modifiers) {
+    socket.on("keyboard-pressKey", (id, key, modifiers) => {
         socket.broadcast.emit("keyboard-pressKey", id, key, modifiers);
     });
 
-    socket.on('mouse-move', function (id, offset) {
+    socket.on("mouse-move", (id, offset) => {
         socket.broadcast.emit("mouse-move", id, offset);
     });
 
-    socket.on('mouse-wheel', function (id, delta) {
+    socket.on("mouse-wheel", (id, delta) => {
         socket.broadcast.emit("mouse-wheel", id, delta);
     });
 
-    socket.on('mouse-hWheel', function (id, delta) {
+    socket.on("mouse-hWheel", (id, delta) => {
         socket.broadcast.emit("mouse-hWheel", id, delta);
     });
 
-    socket.on('mouse-leftClick', function (id) {
+    socket.on("mouse-leftClick", id => {
         socket.broadcast.emit("mouse-leftClick", id);
     });
 
-    socket.on('mouse-rightClick', function (id) {
+    socket.on("mouse-rightClick", id => {
         socket.broadcast.emit("mouse-rightClick", id);
     });
 
-    socket.on('mouse-middleClick', function (id) {
+    socket.on("mouse-middleClick", id => {
         socket.broadcast.emit("mouse-middleClick", id);
     });
 });
 
 
-http.listen(port, function () {
-    console.log('listening on *:' + port);
+httpServer.listen(port, () => {
+    console.log("listening on *:" + port);
 });
