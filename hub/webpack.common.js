35b0e47904101b54f3b827a937da17a7df5bfffc
Thomas <Thomas@DevPad>
2023-03-12 12:58:40 +0100

    Fix package.json rewrite

--- hub/webpack.common.js
+++ hub/webpack.common.js
@@ -40,8 +40,8 @@ const server = {
           transform: (content) => {
             var packageJson = JSON.parse(content.toString());
             packageJson.scripts.start = "node server/index.js";
+            packageJson.scripts.build = "";
             packageJson.main = "server/index.js";
-            packageJson.build = "";
             delete packageJson.devDependencies;
             delete packageJson.jest;
             return Buffer.from(JSON.stringify(packageJson));
