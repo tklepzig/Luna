e829e881821981fcc1babdd9cd025ea9ed6d7bf5
Thomas <Thomas@DevPad>
2023-03-12 13:35:10 +0100

    Use env variable for port

--- hub/src/server/index.ts
+++ hub/src/server/index.ts
@@ -3,7 +3,7 @@ import http from "http";
 import express, { json, urlencoded } from "express";
 import cors from "cors";
 
-const port = 8080;
+const port = process.env.PORT ?? 8080;
 const app = express();
 app.use(cors());
 app.use(json());
